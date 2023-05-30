501d1dd3025218a102365a5f009c001cec45631f
Guohao Dou <guohao.dou@epfl.ch>
2023-05-30 19:30:07 +0200

    hessian for MNIST working

--- main.py
+++ main.py
@@ -3,26 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
+from enum import Enum
+
+
+class Mode(Enum):
+    NoHessian = 0
+    FullHessian = 1
+    DiagHessian = 2
+
+
+mode = Mode.DiagHessian
+
+
 torch.manual_seed(666)
 device = torch.device("cuda")
 
+
 input_width = 28 * 28  # 784
-layer_1_out_width = 32
+layer_1_out_width = 20
 layer_2_out_width = 10
 
 
 def fc2_fwd(fc):
     output = fc.reshape(layer_2_out_width, layer_1_out_width) @ fc2_inp
-    output = F.log_softmax(output, dim=0)
-    return F.nll_loss(output.T, target)
+    return F.cross_entropy(output.T, target)
+    # output = F.log_softmax(output, dim=0)
+    # return F.nll_loss(output.T, target)
 
 
 if __name__ == '__main__':
-    fc1_lr = 0.1
-    fc2_lr = 0.1
-    epochs = 14
-    log_interval = 10
-    train_kwargs = {'batch_size': 64}
+    first_order_lr = 0.01
+    second_order_lr = first_order_lr
+    hessian_boost = 0.01
+    epochs = 10
+    log_interval = 1
+    train_kwargs = {'batch_size': 1000}
     test_kwargs = {'batch_size': 1000}
     cuda_kwargs = {'num_workers': 1,
                    'pin_memory': True,
@@ -62,12 +77,25 @@ if __name__ == '__main__':
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item()))
-            assert fc1.requires_grad and fc2.requires_grad
             with torch.no_grad():
-                fc1 -= fc1_lr * fc1.grad
-                fc2 -= fc2_lr * fc2.grad
+                # with torch.autograd.set_detect_anomaly(True):
+                fc1 -= first_order_lr * fc1.grad
                 fc1.grad = None
+                fc2_grad = fc2.grad
                 fc2.grad = None
+                if mode == Mode.FullHessian:
+                    hessian = torch.autograd.functional.hessian(fc2_fwd, (fc2,), strict=True)[0][0]
+                    hessian = hessian.to(device)
+                    hessian.diagonal().copy_(hessian.diagonal() + hessian_boost)  # boost diagonal for stability
+                    fc2 -= second_order_lr * torch.linalg.lstsq(hessian, fc2_grad).solution  # pseudo inverse
+                elif mode == Mode.DiagHessian:
+                    hessian = torch.autograd.functional.hessian(fc2_fwd, (fc2,), strict=True)[0][0]
+                    hessian = hessian.to(device)
+                    diag_inv_hessian = 1 / (hessian.diagonal() + hessian_boost)
+                    fc2 -= second_order_lr * diag_inv_hessian * fc2_grad
+                else:
+                    assert mode == Mode.NoHessian
+                    fc2 -= first_order_lr * fc2_grad
         # testing
         test_loss = 0
         correct = 0
